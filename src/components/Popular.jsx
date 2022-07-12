5c47625119503c1a5b5f58c6a1f232e8bfdc7d4e
janisollo <sollo.jani@gmail.com>
2022-07-12 11:40:20 +0700

    API search done

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Popular = () => {
-  return <div>Popular</div>;
+  useEffect(() => {
+    getPopular();
+  }, []);
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`,
+    );
+    const data = await api.json();
+    console.log(data);
+  };
+
+  return <div></div>;
 };
 
 export default Popular;
