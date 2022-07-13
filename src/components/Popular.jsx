b58b11e8f0a7aef8225a1bd0b69f549edbd3326c
janisollo <sollo.jani@gmail.com>
2022-07-13 12:22:58 +0700

    Changed api to use local storage to prevent continous searches

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -9,15 +9,21 @@ const Popular = () => {
   useEffect(() => {
     getPopular();
   }, []);
-  const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`,
-    );
-    const data = await api.json();
-    setPopular(data.recipes);
-    console.log(data.recipes);
-  };
 
+  const getPopular = async () => {
+    const check = localStorage.getItem('popular');
+
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`,
+      );
+      const data = await api.json();
+      localStorage.setItem('popular', JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+    }
+  };
   return (
     <div>
       <Wrapper>
@@ -33,7 +39,7 @@ const Popular = () => {
         >
           {popular.map((recipe) => {
             return (
-              <SplideSlide>
+              <SplideSlide key={recipe.id}>
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
