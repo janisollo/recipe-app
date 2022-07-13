cdadde81d415ac0d1b006984d356a37ba662dc3f
janisollo <sollo.jani@gmail.com>
2022-07-13 15:36:53 +0700

    Routing added

--- src/components/Gategory.jsx
+++ src/components/Gategory.jsx
@@ -1,31 +1,67 @@
 import React from 'react';
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
 import { GiNoodles, GiChopsticks } from 'react-icons/gi';
-// import { BsHouseFill } from 'react-icons/bs';
 import styled from 'styled-components';
 // import { NavLink } from 'react-router-dom';
 
 const Gategory = () => {
   return (
-    <div>
+    <List>
       <div>
         <FaPizzaSlice />
         <h4>Italian</h4>
       </div>
       <div>
         <FaHamburger />
-        <h4>Hamburgers</h4>
+        <h4>American</h4>
       </div>
       <div>
         <GiNoodles />
-        <h4>Noodles</h4>
+        <h4>Thai</h4>
       </div>
       <div>
         <GiChopsticks />
-        <h4>Asian</h4>
+        <h4>Japanese</h4>
       </div>
-    </div>
+    </List>
   );
 };
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0rem;
+`;
+// const SLink = styled(NavLink)`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   border-radius: 50%;
+//   margin-right: 2rem;
+//   text-decoration: none;
+//   background: linear-gradient(35deg, #494949, #313131);
+//   width: 6rem;
+//   height: 6rem;
+//   cursor: pointer;
+//   transform: scale(0.8);
+//   h4 {
+//     color: white;
+//     font-size: 0.8rem;
+//   }
+//   svg {
+//     color: white;
+//     font-size: 1.5rem;
+//   }
+//   &.active {
+//     background: linear-gradient(to right, #b3762d, #b35a12);
+//     svg {
+//       color: white;
+//     }
+//     h4 {
+//       color: white;
+//     }
+//   }
+// `;
 
 export default Gategory;
---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cuisine = () => {
+  return (
+    <div>
+      <img src="https://source.unsplash.com/random/200x200?sig=1" alt="" />
+    </div>
+  );
+};
+
+export default Cuisine;
